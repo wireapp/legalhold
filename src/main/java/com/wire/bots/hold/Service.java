73319312d2b2544750006c4d012ff1fccab541a5
dkovacevic <dejankov@gmail.com>
2019-04-23 18:36:11 +0200

    Lithium 2.23.0

--- src/main/java/com/wire/bots/hold/Service.java
+++ src/main/java/com/wire/bots/hold/Service.java
@@ -20,11 +20,6 @@ package com.wire.bots.hold;
 import com.wire.bots.hold.model.Config;
 import com.wire.bots.sdk.MessageHandlerBase;
 import com.wire.bots.sdk.Server;
-import com.wire.bots.sdk.crypto.CryptoDatabase;
-import com.wire.bots.sdk.crypto.storage.RedisStorage;
-import com.wire.bots.sdk.factories.CryptoFactory;
-import com.wire.bots.sdk.factories.StorageFactory;
-import com.wire.bots.sdk.state.RedisState;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -33,7 +28,8 @@ public class Service extends Server<Config> {
     private Database database;
 
     public static void main(String[] args) throws Exception {
-        new Service().run(args);
+        Service instance = new Service();
+        instance.run(args);
     }
 
     @Override
@@ -59,53 +55,12 @@ public class Service extends Server<Config> {
     protected void onRun(Config config, Environment env) {
         LoginResource loginResource = new LoginResource(client,
                 database,
-                getCryptoFactory(config),
+                getCryptoFactory(),
                 config);
 
         addResource(loginResource, env);
 
         Thread thread = new Thread(new NotificationProcessor(client, database, config));
         thread.start();
-    }
-
-//    @Override
-//    protected StorageFactory getStorageFactory(Config config) {
-//        return botId -> new PostgresState(botId, config.db);
-//    }
-//
-//    @Override
-//    protected CryptoFactory getCryptoFactory(Config config) {
-//        return (botId) -> {
-//            Configuration.DB db = config.db;
-//            IStorage storage = new PgStorage(db.user, db.password, db.database, db.host, db.port);
-//            return new CryptoDatabase(botId, storage);
-//        };
-//    }
-
-    /**
-     * Instructs the framework to use Storage Service for the state.
-     * Remove this override in order to use local File system storage
-     *
-     * @param config Config
-     * @return Storage
-     */
-    @Override
-    protected StorageFactory getStorageFactory(Config config) {
-        return botId -> new RedisState(botId, config.db);
-    }
-
-    /**
-     * Instructs the framework to use Crypto Service for the crypto keys.
-     * Remove this override in order to store cryptobox onto your local File system
-     *
-     * @param config Config
-     * @return CryptoFactory
-     */
-    @Override
-    protected CryptoFactory getCryptoFactory(Config config) {
-        return (botId) -> {
-            RedisStorage storage = new RedisStorage(config.db.host, config.db.port, config.db.password);
-            return new CryptoDatabase(botId, storage);
-        };
     }
 }

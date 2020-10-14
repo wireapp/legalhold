019d959db469b78d2c5f313137f48c7b8a6d71bc
dkovacevic <dejankov@gmail.com>
2020-10-14 14:57:22 +0200

    Try/catch in EventsResultSetMapper

--- src/main/java/com/wire/bots/hold/DAO/EventsResultSetMapper.java
+++ src/main/java/com/wire/bots/hold/DAO/EventsResultSetMapper.java
@@ -1,6 +1,7 @@
 package com.wire.bots.hold.DAO;
 
 import com.wire.bots.hold.model.Event;
+import com.wire.bots.sdk.tools.Logger;
 import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.tweak.ResultSetMapper;
 
@@ -12,15 +13,20 @@ public class EventsResultSetMapper implements ResultSetMapper<Event> {
     @Override
     public Event map(int i, ResultSet rs, StatementContext statementContext) throws SQLException {
         Event event = new Event();
-        Object conversationId = rs.getObject("conversationId");
-        if (conversationId != null)
-            event.conversationId = (UUID) conversationId;
-        event.time = rs.getString("time");
-        event.type = rs.getString("type");
-        event.payload = rs.getString("payload");
-        Object messageId = rs.getObject("messageId");
-        if (messageId != null)
-            event.messageId = (UUID) messageId;
+        try {
+            Object conversationId = rs.getObject("conversationId");
+            if (conversationId != null)
+                event.conversationId = (UUID) conversationId;
+            event.time = rs.getString("time");
+            event.type = rs.getString("type");
+            event.payload = rs.getString("payload");
+            Object messageId = rs.getObject("messageId");
+            if (messageId != null)
+                event.messageId = (UUID) messageId;
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.warning("EventsResultSetMapper: %s", e);
+        }
         return event;
     }
 }

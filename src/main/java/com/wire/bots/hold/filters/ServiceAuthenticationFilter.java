b09b3f8c0dfa85ae89ae67736a3ab6c2a7ca698e
dkovacevic <dejankov@gmail.com>
2021-04-12 20:01:59 +0200

    Logging in ServiceAuthenticationFilter

--- src/main/java/com/wire/bots/hold/filters/ServiceAuthenticationFilter.java
+++ src/main/java/com/wire/bots/hold/filters/ServiceAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.wire.bots.hold.filters;
 
 import com.wire.bots.hold.Service;
+import com.wire.xenon.tools.Logger;
+import com.wire.xenon.tools.Util;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -12,7 +14,6 @@ import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
-import java.util.Objects;
 
 @Provider
 public class ServiceAuthenticationFilter implements ContainerRequestFilter {
@@ -29,9 +30,8 @@ public class ServiceAuthenticationFilter implements ContainerRequestFilter {
         if (authHeader == null)
             authHeader = authCookie.getValue();
 
-        final String token = String.format("Bearer %s", Service.instance.getConfig().token);
-
-        if (!Objects.equals(token, authHeader)) {
+        if (!Util.compareAuthorizations(Service.instance.getConfig().token, authHeader)) {
+            Logger.warning("Wrong service token");
             Exception cause = new IllegalArgumentException("Wrong service token");
             throw new WebApplicationException(cause, Response.Status.UNAUTHORIZED);
         }

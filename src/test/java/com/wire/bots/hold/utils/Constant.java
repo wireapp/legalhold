51dee608491e994b6f40cd4bde71922427b54dc9
alexandreferris <ferris.alexandre@gmail.com>
2025-03-12 15:05:20 +0100

    feat(update-publish-plugins-and-xenon-version)
    * Update Helium to 1.6.2 to include changes from Xenon 1.8.2 for MLS Public Key based on received ciphersuite

--- src/test/java/com/wire/bots/hold/utils/Constant.java
+++ src/test/java/com/wire/bots/hold/utils/Constant.java
@@ -2,4 +2,5 @@ package com.wire.bots.hold.utils;
 
 public class Constant {
     public static final Integer DEFAULT_CIPHERSUITE_IDENTIFIER = 1;
+    public static final Integer CUSTOM_CIPHERSUITE_IDENTIFIER = 7;
 }
